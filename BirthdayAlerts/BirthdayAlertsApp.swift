6da0c1568ee67c9c724578934c5d1b2adcb850f9
Lillian Yang <lyang@lcs.on.ca>
2021-11-29 14:26:01 -0500

    Initial Commit

---
+++ BirthdayAlerts/BirthdayAlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAlertsApp.swift
+//  BirthdayAlerts
+//
+//  Created by Lillian Yang on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
